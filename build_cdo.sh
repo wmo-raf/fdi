0bde6c0e49268d59f0b0c54502260ac14b55333e
Erick Otenyo <otenyo.erick@gmail.com>
2022-09-20 20:21:15 +0300

    update

--- build_cdo.sh
+++ build_cdo.sh
@@ -1,5 +1,5 @@
 set -xeu
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
     /bin/bash ~/miniconda.sh -b -p /opt/conda && /opt/conda/bin/conda install -c conda-forge cdo \
     &&  /opt/conda/bin/conda clean -afy
