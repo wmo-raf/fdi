ee19d3ba7ee6140a1e6e9d4c561e32156538e4ed
Erick Otenyo <otenyo.erick@gmail.com>
2022-11-02 21:07:09 +0300

    update mslp units

--- src/datasets/gfs-mean-sea-level-pressure.js
+++ src/datasets/gfs-mean-sea-level-pressure.js
@@ -1,10 +1,9 @@
 export const name = `mean sea level pressure`;
 
 export const metadata = {
-  unit: "hPa",
+  unit: "Pa",
 };
 
 export const grib2_options = {
   match: ":PRMSL:mean sea level",
-  factor: 1e-2,
 };
